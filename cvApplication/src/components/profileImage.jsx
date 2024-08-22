98eba4c0073e3a5ab9d4e2412a75d59a947bdcbe
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-08-22 23:54:36 +0300

    Add a basic styling for the frame of profile img

---
+++ cvApplication/src/components/profileImage.jsx
@@ -0,0 +1,15 @@
+import '../styles/profileImage.css'
+
+
+function Profile() {
+
+  return (
+    <>
+      <section className="profile">
+        <div className="profileImg"></div>
+      </section>
+    </>
+  )
+}
+
+export default Profile
