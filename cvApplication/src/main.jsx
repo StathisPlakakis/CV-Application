f4e864c510a2ad32906dbb758893c0060836e2d3
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-08-24 20:01:36 +0300

    Create generalInfo file

--- cvApplication/src/main.jsx
+++ cvApplication/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Profile from './components/profileImage.jsx'
+import Info from './components/generalInfo.jsx'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Profile />
+    <Info />
   </StrictMode>,
 )
