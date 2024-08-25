8f1424f8e53184c1a4c795a25fa0dab75e5bea17
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-08-25 15:47:38 +0300

    Add and style all inputs for info management

--- cvApplication/src/components/generalInfoDialog.jsx
+++ cvApplication/src/components/generalInfoDialog.jsx
@@ -1,4 +1,5 @@
 import { useRef, useEffect } from "react"
+import '../styles/generalInfoDialog.css'
 
 function InfoDialog({ isOpen, onClose }) {
 
@@ -15,12 +16,52 @@ function InfoDialog({ isOpen, onClose }) {
   return (
     <dialog ref={dialogRef}>
       <div className='infoDialHead'>
-        <h2>Profile photo</h2>
+        <h2>Edit info</h2>
         <button onClick={onClose}></button>
       </div>
-      {/* <form>
+      <form className="infoForm">
+        <div className="infoItems">
 
-      </form> */}
+          <div className="infoItem" tabIndex={0}>
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              id="firstName"
+              name="first-name"
+              required
+            />
+          </div>
+
+          <div className="infoItem" tabIndex={0}>
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              id="lastName"
+              name="last-name"
+              required
+            />
+          </div>
+
+          <div className="infoItem" tabIndex={0}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+            />
+          </div>
+
+          <div className="infoItem" tabIndex={0}>
+            <label htmlFor="phoneNumber">Phone Number</label>
+            <input
+              type="tel"
+              id="phoneNumber"
+              name="phone-number"
+            />
+          </div>
+
+        </div>
+      </form>
     </dialog>
   )
 }
