9c15ae5504261874db1bb08303715ab8fcb4d429
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-08-27 11:50:00 +0300

    Add the boiler plate content of About dialog

--- cvApplication/src/components/aboutDialog.jsx
+++ cvApplication/src/components/aboutDialog.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import '../styles/aboutDialog.css';
 
 function AboutDialog({isDialogOpen}) {
 
@@ -19,7 +20,32 @@ function AboutDialog({isDialogOpen}) {
         <button></button>
       </div>
       <form className="aboutForm">
+        <div className="aboutItems">
+          <div className="aboutItem" tabIndex={0}>
+            <label htmlFor="about">About</label>
+            <textarea
+              id="about"
+              name="about"
+              type="text"
 
+            ></textarea>
+            {/* <input
+              defaultValue={firstName}
+              autoComplete="off"
+              type="text"
+              id="firstName"
+              name="first-name"
+              required
+            /> */}
+          </div>
+        </div>
+        <div className="save">
+          <button 
+            type="submit"
+          >
+            Save
+          </button>
+        </div>
       </form>
     </dialog>
   )
